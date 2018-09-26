699928c8d8baa6e5a30c340f0854c3dfbe1b7219
Chris Eddy <ceddy.zen@gmail.com>
2018-09-26 13:47:14 -0700

    template setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './exokit.svg';
+import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="">
+            <img src={logo} width="50" height="50" className="d-inline-block align-top" alt=""/>
+              Exokit
+          </a>
+        </nav>
+        <ReactTwitchEmbedVideo channel="avaer" width='100%'/>
+        <iframe width="80%" height="400" allowFullScreen="yes" scrolling="no" src="https://chriseddy.github.io/threejs-webvr-solarsystem/"/>
       </div>
     );
   }
